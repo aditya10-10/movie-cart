655fe592e2d2d304486aee9552ad68be3f592cef
Aditya Chauhan <ascs25835@gmail.com>
2023-10-16 20:26:29 +0530

    Added middleware

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
 import React from "react";
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+// import { createStore, getDefaultMiddleware } from '@reduxjs/store';
 import "./index.css";
 import App from "./components/App";
 // import moviesReducer from './reducers';
 import  rootReducer  from "./reducers";
 
+const logger  = function ({dispatch, getState}) {
+  return function (next){
+    return function (action){
+      console.log("action", action.type);
+      next(action);
+    }
+  }
+}  
+
 const store = configureStore({
   reducer: {
     movies: rootReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 console.log('store', store);
